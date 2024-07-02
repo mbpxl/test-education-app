7ee80b6439859593c43034e7a82a38b2e5d75641
dirtyb1rd <yeskov777.888@gmail.com>
2024-07-02 13:43:29 +0300

    feature(answer options) set appropriate background to clicked answer

--- src/components/QuestionsList/QuestionsList.tsx
+++ src/components/QuestionsList/QuestionsList.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 import { DataArrayType, data } from "../../data/data";
-import { List } from "../Styled/StyledList";
+import { List, ListItem } from "../Styled/StyledList";
 
-export const QuestionList = (props: any) => {
+export const QuestionList = (props: {
+  index: number;
+  handleAnswerClick: (answerIndex: number) => void;
+  selectedAnswer: number | null;
+  isCorrect: boolean | null;
+}) => {
   let currentItem = data[props.index];
-  console.log(currentItem);
 
   return (
     <List>
       {Object.keys(currentItem)
         .filter((key) => key.startsWith("option"))
-        .map((key) => (
-          <li>{currentItem[key as keyof DataArrayType]}</li>
-        ))}
+        .map((key, id) => {
+          const isSelected = props.selectedAnswer === id + 1;
+          const correct = isSelected ? props.isCorrect : null;
+
+          return (
+            <ListItem
+              key={id}
+              onClick={() => props.handleAnswerClick(id + 1)}
+              correct={correct}
+            >
+              {currentItem[key as keyof DataArrayType]}
+            </ListItem>
+          );
+        })}
     </List>
   );
 };
--- src/components/Styled/StyledList.tsx
+++ src/components/Styled/StyledList.tsx
@@ -1,19 +1,48 @@
 import styled from "styled-components";
 
-const StyledList = styled.ul`
-  li {
-    display: flex;
-    align-items: center;
-    height: 70px;
-    padding-left: 15px;
-    border: 1px solid #686868;
-    border-radius: 8px;
-    margin-bottom: 20px;
-    font-size: 20px;
-    cursor: pointer;
-  }
+type TypeStyledListItem = {
+  correct: boolean | null;
+  checkAns?: (ans: any) => void;
+};
+
+const StyledList = styled.ul``;
+
+const StyledListItem = styled.li<TypeStyledListItem>`
+  display: flex;
+  align-items: center;
+  height: 70px;
+  padding-left: 15px;
+  border: 1px solid #686868;
+  border-radius: 8px;
+  margin-bottom: 20px;
+  font-size: 20px;
+  cursor: pointer;
+
+  ${(props) =>
+    props.correct &&
+    `
+    background-color: #dffff2;
+    border-color: #00d397;
+    `};
+  ${(props) =>
+    !props.correct &&
+    `
+    background-color: #FFEBEB;
+    border-color: #FF4A4A;
+    `};
+
+  ${(props) =>
+    props.correct === null &&
+    `
+    background-color: transparent;
+    border-color: #686868;
+    `};
 `;
 
 export const List = (props: any) => {
   return <StyledList {...props} />;
+};
+
+export const ListItem = (props: any) => {
+  return <StyledListItem {...props} />;
 };
